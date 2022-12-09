a847e2d79b53e14e2e0bd9ff3aa1b875f74e4270
mannewdan <109920968+mannewdan@users.noreply.github.com>
2022-12-09 01:15:01 -0600

    texture setup

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -7,9 +7,23 @@ type TileProps = {
 export function Tile ({ type } : TileProps) {   
   const { debug } = useDungeonContext();
 
+  const x = 2;
+  const y = 6;
+
+  const posX = x * 100 / 7;
+  const posY = y * 100 / 7;
   return (
       <div className="grid__tile">
-        {debug && type}
+        <div className="grid__tile--ground"></div>
+        <div 
+          className="grid__tile--wall"
+          style={{
+            backgroundPositionX: `${posX}%`,
+            backgroundPositionY: `${posY}%`
+          }}
+        >
+          {debug && type}
+        </div>
       </div>
   )
 }
