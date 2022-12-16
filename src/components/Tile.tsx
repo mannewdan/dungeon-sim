bfb4faca10aba78ba4bc9b17546ffdc1c810ef0d
mannewdan <109920968+mannewdan@users.noreply.github.com>
2022-12-15 20:28:37 -0600

    tile juice

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useDungeonContext } from "../context/DungeonContext";
 
 type TileProps = {
@@ -8,18 +9,27 @@ type TileProps = {
 };
 
 export function Tile({ type, coords, toggleWall, paint }: TileProps) {
-  const { debug } = useDungeonContext();
-
   const posX = (coords.x * 100) / 7;
   const posY = 100 - (coords.y * 100) / 7;
+  const [savedCoords, setSavedCoords] = React.useState({ posX: 0, posY: 0 });
+
+  //update coords only if type is "wall"
+  if (type === 1 && (savedCoords.posX !== posX || savedCoords.posY !== posY)) {
+    setSavedCoords({ posX, posY });
+  }
+
+  const { debug } = useDungeonContext();
+
+  //render
+  const activeClass = type === 1 ? "active" : "";
   return (
     <div className="grid__tile" onMouseDown={toggleWall} onMouseEnter={paint}>
       <div className="grid__tile--ground"></div>
       <div
-        className="grid__tile--wall"
+        className={`grid__tile--wall ${activeClass}`}
         style={{
-          backgroundPositionX: `${posX}%`,
-          backgroundPositionY: `${posY}%`,
+          backgroundPositionX: `${savedCoords.posX}%`,
+          backgroundPositionY: `${savedCoords.posY}%`,
         }}
       >
         {debug && type}
